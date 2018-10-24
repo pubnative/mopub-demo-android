8f07c09c0e3827adaf7409ce4531554694544b09
eros902002@googlemail.com <eros902002@googlemail.com>
2018-10-24 14:38:27 +0200

    Create navigation elements

--- app/src/main/java/net/pubnative/mopubdemo/ui/activities/MainActivity.kt
+++ app/src/main/java/net/pubnative/mopubdemo/ui/activities/MainActivity.kt
@@ -1,35 +1,29 @@
 package net.pubnative.mopubdemo.ui.activities
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import net.pubnative.mopubdemo.R
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.fragment_nav_host)
+        navigation.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragment_nav_host).navigateUp()
     }
 }
--- app/src/main/java/net/pubnative/mopubdemo/ui/fragments/AdUnitNavFragment.kt
+++ app/src/main/java/net/pubnative/mopubdemo/ui/fragments/AdUnitNavFragment.kt
@@ -1,6 +1,14 @@
 package net.pubnative.mopubdemo.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import net.pubnative.mopubdemo.R
 
 class AdUnitNavFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_nav_ad_unit, container, false)
+    }
 }
