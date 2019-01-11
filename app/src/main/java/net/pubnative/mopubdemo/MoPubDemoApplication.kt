0c463b6ca5379f06811bef3603ec424f75aaf1a3
eros902002@googlemail.com <eros902002@googlemail.com>
2019-01-11 12:02:43 +0100

    Set MoPub Log level to all

--- app/src/main/java/net/pubnative/mopubdemo/MoPubDemoApplication.kt
+++ app/src/main/java/net/pubnative/mopubdemo/MoPubDemoApplication.kt
@@ -1,9 +1,13 @@
 package net.pubnative.mopubdemo
 
 import android.app.Application
+import com.mopub.common.logging.MoPubLog
+import java.util.logging.Level
 
 class MoPubDemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        MoPubLog.setSdkHandlerLevel(Level.ALL)
     }
 }
